09d687bd89b83a3a27205b7cb1c4ca1e57916013
SYSU_YZT <2543761065@qq.com>
2018-12-06 13:06:30 +0800

    O(n) 寻找第k大的数

---
+++ Data_Structure_And_Algorithm/教务一班Sicily/Search/1000.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+int n, k, temp;
+int A[10000005];
+int h[20000005] = { 0 };//有毛区别啊？ 这一行变成h[1000000005]就不行？
+
+void swap(int *a, int *b) {
+	if (a != b) {
+		int temp = *a;
+		*a = *b;
+		*b = temp;
+	}
+}
+
+int partion(int s, int e) {
+	int j = s - 1;
+	for (int i = s; i < e; i++) {
+		if (A[i] < A[e] && i != j) {		//全顺序这条if不会执行，该partion以A[e]分界，返回A[e]的位置
+			j++;
+			swap(&A[i], &A[j]);
+		}
+	}
+	swap(&A[j + 1], &A[e]);
+	return j + 1;
+}
+
+int quick(int s, int e) {
+	int m = partion(s, e);
+	if (m == k)return A[m];
+	else if (m > k) return quick(s, m - 1);
+	else return quick(m + 1, e);
+}
+
+int main() {
+	int count = 0;
+	cin >> n >> k;
+	for (int i = 0; i < n; i++) {
+		cin >> temp;
+		if (!h[temp+ 10000000]) {
+			h[temp+ 10000000] = 1;
+			A[count++] = temp;
+		}
+	}
+	k = count - k;
+	cout << quick(0, count - 1) << endl;
+	return 0;
+}
