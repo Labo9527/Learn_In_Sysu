50e88e5854fae02b096ea2e385ddb44a864211d9
SYSU_YZT <2543761065@qq.com>
2019-02-15 12:43:13 +0800

    刚才提交不完整

---
+++ Web_2.0/angular-express-blog-master/public/js/app.js
@@ -0,0 +1,39 @@
+'use strict';
+
+// Declare app level module which depends on filters, and services
+angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
+  config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'partials/index',
+        controller: IndexCtrl
+      }).
+      when('/signin',{
+        templateUrl: 'partials/addPost',
+        controller: AddPostCtrl             //先把sign in做成addPost
+      }).
+      when('/signup',{
+        templateUrl: 'partials/addPost',
+        controller: AddPostCtrl             //先把sign up做成addPost
+      }).
+      when('/addPost', {
+        templateUrl: 'partials/addPost',
+        controller: AddPostCtrl
+      }).
+      when('/readPost/:id', {
+        templateUrl: 'partials/readPost',
+        controller: ReadPostCtrl
+      }).
+      when('/editPost/:id', {
+        templateUrl: 'partials/editPost',
+        controller: EditPostCtrl
+      }).
+      when('/deletePost/:id', {
+        templateUrl: 'partials/deletePost',
+        controller: DeletePostCtrl
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+    $locationProvider.html5Mode(true);
+  }]);
